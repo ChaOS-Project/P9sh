e4cb4899c8f89454108aef0e6dc868be0e0ba8a1
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2011-11-05 12:21:12 +0100

    Read from stdin and write to stdout with bio(2)

--- src/shell.c
+++ src/shell.c
@@ -11,9 +11,43 @@
 #include <u.h>
 #include <libc.h>
 
+#include <bio.h>
+
+
 void
 main(int argc, char* argv[])
 {
-	print("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	Biobuf*	bin;
+	Biobuf*	bout;
+	char*	line;
+
+	// Allocate buffer for standard input
+	bin = Bfdopen(0, O_RDONLY);
+	if(bin == nil)
+		sysfatal("%s: standard input: %r", argv0);
+
+	// Allocate buffer for standard output
+	bout = Bfdopen(1, O_WRONLY);
+	if(bout == nil)
+		sysfatal("%s: standard output: %r", argv0);
+
+	// Main loop
+	for(;;)
+	{
+		line = Brdstr(bin, '\n', 0);
+		if(line)
+		{
+			Bwrite(bout, line, Blinelen(bin));
+			Bflush(bout);
+
+			free(line);
+		}
+	}
+
+	// Free buffers
+	Bterm(bin);
+	Bterm(bout);
+
+	// Exit sucesfully
 	exits(nil);
 }
