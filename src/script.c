f24383e788f6f9ad6ba1e4666caed351d60d4f9b
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2011-11-14 03:28:32 +0100

    Expand vars previously to command processing

--- src/script.c
+++ src/script.c
@@ -15,62 +15,138 @@
 #include "pipeline.h"
 
 
+void
+do_expand(char** expanded, char* key)
+{
+	if(key[0])
+	{
+		char* env = getenv(key);
+		if(env)
+		{
+			strcat(*expanded, env);
+			free(env);
+		}
+		else
+		{
+			strcat(*expanded, "$");
+			strcat(*expanded, key);
+		}
+	}
+}
+
 char*
 expand_envVars(char* line)
+// alternative function to expand environment variables using a state machine
+// since regular expression functions are not working correctly
 {
-	line = "ls";
+	char* expanded = malloc(1024 * 10 * sizeof(char));
+	strncpy(expanded, "", 1024 * 10 * sizeof(char));
 
-	// Compile regular expression
-	char reg[] = "[a-zA-Z_]+";
-//	char reg[] = "\\$[a-zA-Z_]+";
-	Reprog* prog = regcomp(reg);
+	char key[32];
+	strncpy(key, "", 32);
 
-	Resub sub[16];
+	int mode = 0;
 
-	print("nelem(sub): %d\n", nelem(sub));
-	print("regexec: %d\n", regexec(prog, line, sub, nelem(sub)));
-
-	char* expanded = nil;
-
-	// Exec regular expression until there's no match
-	while(regexec(prog, line, sub, nelem(sub)))
+	for(; *line; ++line)
 	{
-		print("expanded: %s\n",expanded);
-		if(expanded == nil)
+		char c = *line;
+
+		if(c == '$')
 		{
-			print("5");
-			expanded = malloc(1024 * 10 * sizeof(char));
-			*expanded = '\0';
-			print("6");
+			// expand previous one (if any)
+			do_expand(&expanded, key);
+
+			// prepare new expansion
+			strncpy(key, "", 32);
+			mode = 1;
 		}
 
-		// Concat normal string from begining (if any)
-		if(line < sub[0].s.sp)
-			strncat(expanded, line, sub[0].s.sp - line);
-		print("7");
+		else if(('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || c == '_')
+		{
+			char* buf = mode? key: expanded;
 
-		// Concat environment var
-		char match[256];
-		strncpy(match, sub[0].s.sp, sub[0].e.ep - sub[0].s.sp);
+			buf[strlen(buf)] = c;
+			buf[strlen(buf)] = '\0';
+		}
 
-		char* env = getenv(match);
-		strncat(expanded, env, strlen(env));
-		free(env);
-		print("8");
+		else
+		{
+			if(mode == 0)
+			{
+				expanded[strlen(expanded)] = c;
+				expanded[strlen(expanded)] = '\0';
+			}
+			else
+			{
+				do_expand(&expanded, key);
 
-		// Advance line pointer to catch the next match (if any)
-		line = sub[0].e.ep;
+				strncpy(key, "", 32);
+				mode = 0;
+			}
+		}
 	}
-	print("9");
 
-	// Concat remaining normal string after last match (if any)
-	strncat(expanded, line, strlen(line));
-
-	// Free compiled regular expression
-	free(prog);
+	do_expand(&expanded, key);
 
 	return expanded;
 }
+
+//char*
+//expand_envVars(char* line)
+//{
+//	line = "ls";
+//
+//	// Compile regular expression
+//	char reg[] = "[a-zA-Z_]+";
+////	char reg[] = "\\$[a-zA-Z_]+";
+//	Reprog* prog = regcomp(reg);
+//
+//	Resub sub[16];
+//
+//	print("nelem(sub): %d\n", nelem(sub));
+//	print("regexec: %d\n", regexec(prog, line, sub, nelem(sub)));
+//
+//	char* expanded = nil;
+//
+//	// Exec regular expression until there's no match
+//	while(regexec(prog, line, sub, nelem(sub)))
+//	{
+//		print("expanded: %s\n",expanded);
+//		if(expanded == nil)
+//		{
+//			print("5");
+//			expanded = malloc(1024 * 10 * sizeof(char));
+//			*expanded = '\0';
+//			print("6");
+//		}
+//
+//		// Concat normal string from begining (if any)
+//		if(line < sub[0].s.sp)
+//			strncat(expanded, line, sub[0].s.sp - line);
+//		print("7");
+//
+//		// Concat environment var
+//		char match[256];
+//		strncpy(match, sub[0].s.sp, sub[0].e.ep - sub[0].s.sp);
+//
+//		char* env = getenv(match);
+//		strncat(expanded, env, strlen(env));
+//		free(env);
+//		print("8");
+//
+//		// Advance line pointer to catch the next match (if any)
+//		line = sub[0].e.ep;
+//	}
+//	print("9");
+//
+//	// Concat remaining normal string after last match (if any)
+//	strncat(expanded, line, strlen(line));
+//
+//	// Free compiled regular expression
+//	free(prog);
+//
+//	return expanded;
+//}
 
 
 int
@@ -118,6 +194,7 @@ int
 builtin_cd(char* line)
 {
 	line = strdup(line);
+	int ret = 0;
 
 	char* array[10];
 	int numTokens = tokenize(line, array, 10);
@@ -126,12 +203,11 @@ builtin_cd(char* line)
 	{
 		cd(numTokens, array);
 
-		free(line);
-		return 1;
+		ret = 1;
 	}
 
 	free(line);
-	return 0;
+	return ret;
 }
 
 int
@@ -167,10 +243,11 @@ process_script(char* line)
 	int i;
 	for(i = 0; i < numPipelines; ++i)
 	{
-//		// expand environment variables and substitute command line char pointer
-//		// with the one with expanded environment variables (if necesary)
+		// expand environment variables and substitute command line char pointer
+		// with the one with expanded environment variables (if necesary)
 //		char* expanded = expand_envVars(array[i]);
 //		if(expanded) array[i] = expanded;
+		array[i] = expand_envVars(array[i]);
 
 		// move commands to background (if necesary)
 		background(array[i]);
@@ -213,8 +290,8 @@ process_script(char* line)
 			}
 		}
 
-//		// free line created by environment variables expansion
-//		free(expanded);
+		// free line created by environment variables expansion
+		free(array[i]);
 	}
 
 	return 0;
